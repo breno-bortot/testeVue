17ebe5197b5d7e3457b6ffd5e6a528050517665f
breno-bortot <brenobortot@gmail.com>
2021-11-19 22:19:30 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,45 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container pt-3 pb-5">
+    <h2></h2>
+
+    <b-table striped hover :operadoras="operadoras"></b-table>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import papa from "papaparse";
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      operadoras: [],
+      items: [
+        { age: 40, first_name: "Dickerson", last_name: "Macdonald" },
+        { age: 21, first_name: "Larsen", last_name: "Shaw" },
+        { age: 89, first_name: "Geneva", last_name: "Wilson" },
+        { age: 38, first_name: "Jami", last_name: "Carney" },
+      ],
+    };
+  },
+  async created() {
+    this.operadoras = await this.fetchOperadoras();
+  },
+  methods: {
+    async fetchOperadoras() {
+      const result = await fetch("dados/Relatorio_cadop.csv");
+      const data = await result.text();
+      const csvToJson = papa.parse(data, {
+        header: true,
+        dynamicTyping: true,
+        skipEmptyLines: true,
+        complete(result) {
+          return result.data;
+        },
+      });
+      // console.log(csvToJson.data);
+      return csvToJson.data;
+    },
   },
 };
 </script>
