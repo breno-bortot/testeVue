7d988249ea256034cf5ea4448dac9261658a8086
breno-bortot <brenobortot@gmail.com>
2021-11-20 19:50:51 -0300

    All SearchBars rendering

--- src/components/SearchPanel.vue
+++ src/components/SearchPanel.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="container" v-if="tableHead">
     <SearchBar
-      v-for="(data, i) in tableHead.__parsed_extra[0]"
+      v-for="(data, i) in tableHead.__parsed_extra"
       :key="i"
       :label="tableHead.__parsed_extra[i]"
       @search-submit="getSearchInput"
     />
 
-    <!-- <SearchBar :label="tableHead" @search-submit="getSearchInput" /> -->
+    <SearchBar
+      :label="tableHead['Rela��o de Operadoras Ativas ANS']"
+      @search-submit="getSearchInput"
+    />
   </div>
 </template>
 
