5803e24b16b63fecb8a344bd595b74da203fd27b
breno-bortot <brenobortot@gmail.com>
2021-11-19 23:28:40 -0300

    implementing table using converted csvToJson data

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,52 @@
   <div class="container pt-3 pb-5">
     <h2></h2>
 
-    <b-table striped hover :operadoras="operadoras"></b-table>
+    <table class="table table-bordered table-hover table-condensed">
+      <thead>
+        <tr>
+          <th>{{ tableHead.__parsed_extra[0] }}</th>
+          <th>{{ tableHead.__parsed_extra[1] }}</th>
+          <th>{{ tableHead.__parsed_extra[2] }}</th>
+          <th>{{ tableHead.__parsed_extra[3] }}</th>
+          <th>{{ tableHead.__parsed_extra[4] }}</th>
+          <th>{{ tableHead.__parsed_extra[5] }}</th>
+          <th>{{ tableHead.__parsed_extra[6] }}</th>
+          <th>{{ tableHead.__parsed_extra[7] }}</th>
+          <th>{{ tableHead.__parsed_extra[8] }}</th>
+          <th>{{ tableHead.__parsed_extra[9] }}</th>
+          <th>{{ tableHead.__parsed_extra[10] }}</th>
+          <th>{{ tableHead.__parsed_extra[11] }}</th>
+          <th>{{ tableHead.__parsed_extra[12] }}</th>
+          <th>{{ tableHead.__parsed_extra[13] }}</th>
+          <th>{{ tableHead.__parsed_extra[14] }}</th>
+          <th>{{ tableHead.__parsed_extra[15] }}</th>
+          <th>{{ tableHead.__parsed_extra[16] }}</th>
+          <th>{{ tableHead.__parsed_extra[17] }}</th>
+        </tr>
+      </thead>
+      <tbody v-for="(data, i) in operadoras" :key="i">
+        <tr v-if="i !== 0">
+          <td>{{ data.__parsed_extra[0] }}</td>
+          <td>{{ data.__parsed_extra[1] }}</td>
+          <td>{{ data.__parsed_extra[2] }}</td>
+          <td>{{ data.__parsed_extra[3] }}</td>
+          <td>{{ data.__parsed_extra[4] }}</td>
+          <td>{{ data.__parsed_extra[5] }}</td>
+          <td>{{ data.__parsed_extra[6] }}</td>
+          <td>{{ data.__parsed_extra[7] }}</td>
+          <td>{{ data.__parsed_extra[8] }}</td>
+          <td>{{ data.__parsed_extra[9] }}</td>
+          <td>{{ data.__parsed_extra[10] }}</td>
+          <td>{{ data.__parsed_extra[11] }}</td>
+          <td>{{ data.__parsed_extra[12] }}</td>
+          <td>{{ data.__parsed_extra[13] }}</td>
+          <td>{{ data.__parsed_extra[14] }}</td>
+          <td>{{ data.__parsed_extra[15] }}</td>
+          <td>{{ data.__parsed_extra[16] }}</td>
+          <td>{{ data.__parsed_extra[17] }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -14,16 +59,15 @@ export default {
   data() {
     return {
       operadoras: [],
-      items: [
-        { age: 40, first_name: "Dickerson", last_name: "Macdonald" },
-        { age: 21, first_name: "Larsen", last_name: "Shaw" },
-        { age: 89, first_name: "Geneva", last_name: "Wilson" },
-        { age: 38, first_name: "Jami", last_name: "Carney" },
-      ],
     };
   },
   async created() {
     this.operadoras = await this.fetchOperadoras();
+  },
+  computed: {
+    tableHead() {
+      return this.operadoras[0];
+    },
   },
   methods: {
     async fetchOperadoras() {
